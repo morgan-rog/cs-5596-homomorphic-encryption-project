3993f45f6acacfc068fc901c4c17bad24ae68619
Morgan Rogers <morgan.rog@yahoo.com>
2022-10-23 16:51:29 -0500

    added weights file and python file that will use Pyfhel library to encrypt the weights

---
+++ encrypt-smashed-data.py
@@ -0,0 +1,14 @@
+# %%
+# Imports
+import numpy as np
+from Pyfhel import Pyfhel
+
+# %%
+print("1. load in data from smashed_data.npy")
+smashed_data = np.load("smashed_data.npy")
+
+print("Printing array: \n", smashed_data)
+
+# %%
+print("shape of the array: ", smashed_data.shape)
+# %%
