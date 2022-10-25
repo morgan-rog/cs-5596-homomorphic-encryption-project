0bd934c836b8e5f817bd90cb1e31c267252b61e9
Morgan Rogers <morgan.rog@yahoo.com>
2022-10-24 19:50:23 -0500

    looping through and trying to encrypt a single number and an array

--- encrypt-smashed-data.py
+++ encrypt-smashed-data.py
@@ -15,22 +15,14 @@ print("3. shape of the array: ", smashed_data.shape)
 # %%
 print("GENERATING PYFHEL CONTEXT AND KEY SETUP")
 HE = Pyfhel()
-HE.contextGen(scheme='bfv')
+HE.contextGen(scheme='bfv', n=2**14, t_bits=20)
 HE.keyGen()
 print("1. printing the HE object: ")
 print(HE)
 
 # %%
-for i, x  in enumerate(smashed_data):
-    print(i, "--->", x)
-    
-# %%
 print(smashed_data[0][0][0][0])
-
-# %%
 print(smashed_data[0][0][0])
-
-# %%
 print(smashed_data[0][0])
 
 # %%
@@ -38,18 +30,33 @@ for x in smashed_data:
     for y in x:
         for z in y:
             for num in z:
+                print("---------------------")
                 print(num)
                 encrypted_num = HE.encrypt(num)
                 print(encrypted_num)
                 decrypted_num = HE.decrypt(encrypted_num)
                 print(decrypted_num)
                 print(len(decrypted_num))
+                print("---------------------")
                 break
             break
         break
     break
-
-
+# %%
+for x in smashed_data:
+    for y in x:
+        for z in y:
+            print("---------------------")
+            print(z)
+            encrypted_z = HE.encrypt(z)
+            print(encrypted_z)
+            decrypted_z = HE.decrypt(encrypted_z)
+            print(decrypted_z)
+            print(len(decrypted_z))
+            print("---------------------")
+            break
+        break
+    break
 
 # %%
 def encrypt_data(W):
